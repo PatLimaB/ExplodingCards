17d4bd96783920fb6d583c4d17d9de1ac0c80454
PatLimaB <119526100+PatLimaB@users.noreply.github.com>
2024-10-10 16:37:27 +0200

    Update constants.js
    Fixed an error which caused that points value could be 11, but the maximum is 10 points

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,5 +3,5 @@ export const cards = [
     { type: 'Defuse', value: null, count: 6 },
     { type: 'Skip turn', value: null, count: 10 },
     { type: 'Nope', value: null, count: 5 },
-    { type: 'Points', valueRange: [1, 10], count: 35 }
+    { type: 'Points', valueRange: [1, 9], count: 35 }
 ];
